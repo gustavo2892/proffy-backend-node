0c228f04fee3fce573c7b7a3fbd443f80cbcf8ea
gustavo2892 <gustavo2892@gmail.com>
2020-08-08 13:15:20 -0300

    Create and List Connections

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 
 const classesControllers = new ClassesController();
+const connectionsControllers = new ConnectionsController();
 
 routes.post('/classes', classesControllers.create);
 routes.get('/classes', classesControllers.index);
+
+routes.get('/connections', connectionsControllers.index);
+routes.post('/connections', connectionsControllers.create);
 
 export default routes;
